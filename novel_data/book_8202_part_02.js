ed46314ac6cb6b2456c64a84aa4e2841ecc9e89e
UGentertainment <UGentertainment@users.noreply.github.com>
2026-09-02 02:40:11 +0800

    Add Hnovel books 7985-8234

---
+++ novel_data/book_8202_part_02.js
@@ -0,0 +1,2 @@
+globalThis.NOVEL_PAYLOADS=globalThis.NOVEL_PAYLOADS||{};
+globalThis.NOVEL_PAYLOADS["book_8202_part_02"]="H4sIAAAAAAAC/wEhAN7/CgrnrKwxNDTnq6DjgIHmgJzmg5zvvIjkuInmm7TvvIkKD3MX5yEAAAA=";
