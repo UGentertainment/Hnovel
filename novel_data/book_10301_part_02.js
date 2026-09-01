30467d6d80212f12842438eb71827deccdb95695
UGentertainment <UGentertainment@users.noreply.github.com>
2026-09-02 02:44:27 +0800

    Add Hnovel books 10235-10484

---
+++ novel_data/book_10301_part_02.js
@@ -0,0 +1,2 @@
+globalThis.NOVEL_PAYLOADS=globalThis.NOVEL_PAYLOADS||{};
+globalThis.NOVEL_PAYLOADS["book_10301_part_02"]="H4sIAAAAAAAC/3vU0Pyoofn5mjVPdvU8X73gxf4ZzzYtfNa8+enSvVyPwHJPlzU9ndDyZG/vs86GF8sb3+/pebF/5pMdDc/mrHm6YeOzGesfNSx7tn3h06ldcPL5rBYuAA7pvm9ZAAAA";
